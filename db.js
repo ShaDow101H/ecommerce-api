43e5f7be96921c25d2dc71dc42911bc796d455d6
Umid <umustafali68@gmail.com>
2026-03-28 19:11:53 +0400

    password changed.

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "AA12@3RDRd",
+  password: "hope_1234",
   database: "ecommerce_db"
 });
 
