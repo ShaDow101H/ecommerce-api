21a1933f5301de3e331a4b6de4bf662b4dc2721c
Umid <umustafali68@gmail.com>
2026-03-28 18:37:47 +0400

    Initial backend setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const db = require("./db");
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
