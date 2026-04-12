c10b7ef07efa76892802d9bbb02f07e913888e5a
Umid <umustafali68@gmail.com>
2026-04-12 18:13:40 +0400

    server.js edited.

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const db = require("./db");
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const categoryRoutes = require("./routes/categoryRoutes");
 
 app.use(express.json());
+app.use("/api/categories", categoryRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server is running");
