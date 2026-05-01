e86626ab40d20b38f567713620c8443abec22b6c
Umid <umustafali68@gmail.com>
2026-05-01 17:04:18 +0400

    edited

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-const db = require("./db");
-const express = require('express');
+const express = require("express");
+const categoryRoutes = require("./routes/categoryRoutes");
+const authRoutes = require("./routes/authRoutes");
+//const productRoutes = require("./routes/productRoutes");
+
 const app = express();
 const PORT = 3000;
-const categoryRoutes = require("./routes/categoryRoutes");
 
-const productRoutes = require("./routes/productRoutes");
-
+app.use(express.json());
 app.use((req, res, next) => {
   console.log("METHOD:", req.method);
   console.log("URL:", req.url);
   console.log("BODY:", req.body);
-  next(); // Bu mütləq olmalıdır ki, sorğu dayanmasın, davam etsin.
+  next();
 });
-app.use("/categories", categoryRoutes);
-
-app.use("/products", productRoutes);
-
 app.get("/", (req, res) => {
-  res.send("Server is running");
+  res.send("E-commerce API is running");
 });
+
+app.use("/categories", categoryRoutes);
+app.use("/", authRoutes);
+//app.use("/products", productRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
